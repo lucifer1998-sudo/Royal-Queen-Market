8731e98da5c103e47bb8826542b07c20ef26bbf6
Pufferfish <esquilla13@gmail.com>
2021-04-01 00:23:49 +0800

    coupon library

---
+++ vendor/beyondcode/laravel-vouchers/src/Exceptions/VoucherAlreadyRedeemed.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BeyondCode\Vouchers\Exceptions;
+
+use BeyondCode\Vouchers\Models\Voucher;
+
+class VoucherAlreadyRedeemed extends \Exception
+{
+    protected $message = 'The voucher was already redeemed.';
+
+    protected $voucher;
+
+    public static function create(Voucher $voucher)
+    {
+        return new static($voucher);
+    }
+
+    public function __construct(Voucher $voucher)
+    {
+        $this->voucher = $voucher;
+    }
+
+    public function flashError()
+    {
+        session() -> flash('errormessage', $this -> message);
+    }
+}
