26fd559e3cdfaf09447730ba1714a256c9b06ebf
Pufferfish <RAm@some.com>
2021-04-25 00:54:07 +0800

    profie pre layout

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -58,7 +58,7 @@ class ProfileController extends Controller
     }
 
     public function index(){
-        return view('profile.index');
+        return view('tailwind-ui.profile.index');
     }
 
     /**
