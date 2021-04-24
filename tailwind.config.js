dd472f837e86dd7f5fb7faf74877877e866fc5ef
Pufferfish <RAm@some.com>
2021-04-24 16:49:06 +0800

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'rqm-light': '#2a202b',
+            'rqm-lighter': '#2c212d',
+            'rqm-dark': '#211922'
+        }
+    },
   },
   variants: {
     extend: {},
