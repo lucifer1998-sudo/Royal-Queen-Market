8e46b0a18ddbf62f4d47f9ea028f7d964afb21a0
Pufferfish <RAm@some.com>
2021-04-24 17:46:19 +0800

    add body background dark

--- resources/views/tailwind-ui/master/main.blade.php
+++ resources/views/tailwind-ui/master/main.blade.php
@@ -17,7 +17,7 @@
     <link href="{{ asset('css/nordic.css') }}" rel="stylesheet">
 
 </head>
-<body class="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
+<body class="bg-rqm-lighter text-gray-600 work-sans leading-normal text-base tracking-normal">
 @include('tailwind-ui.master.navbar')
 @yield('hero')
 <section class="bg-white py-8">
