0888b207256536f4c9026419e5c0f1c494302d69
Pufferfish <RAm@some.com>
2021-04-24 17:47:09 +0800

    add new color yellow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
         colors: {
             'rqm-light': '#2a202b',
             'rqm-lighter': '#2c212d',
-            'rqm-dark': '#211922'
+            'rqm-dark': '#211922',
+            'rqm-yellow': '#ffc888',
+            'rqm-yellow-dark': '#ffbc6e'
         }
     },
   },
