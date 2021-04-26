10041c7b7afa2a9e863dcdcce9de20a691a77d64
Pufferfish <RAm@some.com>
2021-04-26 17:57:14 +0800

    bottom logo

--- resources/views/tailwind-ui/profile/index.blade.php
+++ resources/views/tailwind-ui/profile/index.blade.php
@@ -24,4 +24,12 @@
             @endif
         </div>
     </div>
+    <div class="flex justify-center w-full">
+        <div class="pl-10 w-1/6 h-full">
+            <span></span>
+        </div>
+        <div class="flex h-full justify-center pl-10 w-5/6">
+            <img src="{{URL::asset('/media/bottom-separator-1.png')}}" class="h-1/3 transform w-2/4" alt="">
+        </div>
+    </div>
 @endsection
