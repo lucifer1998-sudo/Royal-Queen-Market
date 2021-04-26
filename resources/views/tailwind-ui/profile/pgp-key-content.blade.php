7f0ae35d8c58e0a9c577e59b926277939fd85d33
Pufferfish <RAm@some.com>
2021-04-26 18:30:20 +0800

    pgp key layout

--- resources/views/tailwind-ui/profile/pgp-key-content.blade.php
+++ resources/views/tailwind-ui/profile/pgp-key-content.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-rqm-dark content-center flex flex-wrap h-full justify-center p-5 rounded shadow w-full">
-    <div>
+    <div class="w-full">
         @if(auth() -> user() -> hasPGP())
             <p class="mb-5 text-base text-rqm-yellow-darkest text-white">Your PGP key is:</p>
             <div class="border border-rqm-yellow-darkest p-3 shadow-2xl"><p>{!! nl2br(auth() -> user() -> pgp_key) !!}</p></div>
@@ -8,7 +8,7 @@
             <span class="block">Please set the PGP key in the form below.</span>
         @endif
     </div>
-    <div class="flex justify-center py-9 w-1/2">
+    <div class="flex justify-center py-9 w-full">
         <div class="w-full">
             <p class="mb-5 text-base text-rqm-yellow-darkest text-white w-full">Set new PGP </p>
             @if ($errors->any())
@@ -22,7 +22,7 @@
             @endif
             <form class="w-full" method="POST" action="{{ route('profile.pgp.post') }}">
                 {{ csrf_field() }}
-                <textarea name="newpgp" id="newpgp" style="resize: none" rows="10" class="w-full"></textarea>
+                <textarea name="newpgp" id="newpgp" style="resize: none" rows="10" class="bg-rqm-dark border border-rqm-yellow-darkest p-3 text-rqm-yellow w-full" placeholder="Paste here.."></textarea>
                 <small class="text-rqm-yellow-dark">Paste your key here and later you'll need to confirm.</small>
                 <div class="flex py-1 justify-end">
                     <button type="submit" class="bg-rqm-yellow-dark font-extrabold px-3 py-1 rounded-sm text-rqm-dark">
