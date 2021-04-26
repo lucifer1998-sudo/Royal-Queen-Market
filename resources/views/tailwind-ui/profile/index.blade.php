88dedc17204e071eae7be3380bd83daff377b97f
Pufferfish <RAm@some.com>
2021-04-26 17:30:16 +0800

    add header logo

--- resources/views/tailwind-ui/profile/index.blade.php
+++ resources/views/tailwind-ui/profile/index.blade.php
@@ -3,14 +3,25 @@
 @section('title', 'Profile settings')
 
 @section('content')
+    <div class="flex justify-center w-full">
+        <div class="pl-10 w-1/6 h-full">
+            <span></span>
+        </div>
+        <div class="flex h-full justify-center pl-10 w-5/6">
+            <img src="{{URL::asset('/media/bottom-separator-1.png')}}" class="h-1/3 rotate-180 transform w-2/4" alt="">
+        </div>
+    </div>
     <div class="flex">
+
         @include('tailwind-ui.profile.menu')
-        @if(\Route::currentRouteName() == 'profile.index')
-            @include('tailwind-ui.profile.settings-content')
-        @elseif(\Route::currentRouteName() == 'profile.pgp')
-            @include('tailwind-ui.profile.pgp-key-content')
-        @elseif(\Route::currentRouteName() == 'profile.vendor')
-            @include('tailwind-ui.profile.vendor-content')
-        @endif
+        <div class="pl-10 w-5/6 h-full">
+            @if(\Route::currentRouteName() == 'profile.index')
+                @include('tailwind-ui.profile.settings-content')
+            @elseif(\Route::currentRouteName() == 'profile.pgp')
+                @include('tailwind-ui.profile.pgp-key-content')
+            @elseif(\Route::currentRouteName() == 'profile.vendor')
+                @include('tailwind-ui.profile.vendor-content')
+            @endif
+        </div>
     </div>
 @endsection
