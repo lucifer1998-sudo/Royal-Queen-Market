f25860f4486097f9cc0b240b02eca3bf38d438bb
Pufferfish <RAm@some.com>
2021-04-28 23:12:56 +0800

    fix menu dashboard link

--- resources/views/tailwind-ui/admin/menu.blade.php
+++ resources/views/tailwind-ui/admin/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-rqm-dark py-10 rounded w-1/6 relative min-h-screen">
-    <div class="py-2 pl-3 text-rqm-yellow-dark hover:bg-pink-800 hover:shadow @isnotroute('profile.index') hover:grow @endisnotroute @isroute('profile.index')transform scale-105 bg-pink-800 shadow @endisroute">
-        <a class="flex w-full" href="{{ route('profile.index') }}">
+    <div class="py-2 pl-3 text-rqm-yellow-dark hover:bg-pink-800 hover:shadow @isnotroute('admin.index') hover:grow @endisnotroute @isroute('admin.index')transform scale-105 bg-pink-800 shadow @endisroute">
+        <a class="flex w-full" href="{{ route('admin.index') }}">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
             </svg>
