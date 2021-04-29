54fa6d825b03f7ec98414b3961c1462cb031e13c
Pufferfish <RAm@some.com>
2021-04-30 01:14:55 +0800

    product tabs

--- resources/views/tailwind-ui/admin/product-edit-content.blade.php
+++ resources/views/tailwind-ui/admin/product-edit-content.blade.php
@@ -7,6 +7,34 @@
             <div class="flex justify-center text-2xl text-rqm-yellow">
                 Editing product {{$basicProduct->name}}
             </div>
+
+            <div class="flex py-5">
+                <div class="gap-4 grid grid-cols-4 grid-flow-col grid-rows-1 w-full">
+                    <a href="{{ route('admin.product.edit', $basicProduct) }}">
+                        <div class="@if($section == 'basic') bg-rqm-yellow text-rqm-dark @else text-rqm-yellow-dark  bg-rqm-light @endif p-3 rounded ">Basic Information</div>
+                    </a>
+                    <a href="{{ route('admin.product.edit', [$basicProduct, 'offers']) }}">
+                        <div class="@if($section == 'offers') bg-rqm-yellow text-rqm-dark @else text-rqm-yellow-dark  bg-rqm-light @endif p-3 rounded ">Price and Offers</div>
+                    </a>
+                    @php
+                        if(empty($type))
+                            $type = session('product_type');
+                    @endphp
+                    @if($type == 'physical')
+                        <a href="{{ route('admin.product.edit', [$basicProduct, 'delivery']) }}">
+                            <div class="@if($section == 'delivery') bg-rqm-yellow text-rqm-dark @else text-rqm-yellow-dark  bg-rqm-light @endif p-3 rounded">Delivery Options</div>
+                        </a>
+                    @else
+                        <a href="{{ route('admin.product.edit', [$basicProduct, 'digital']) }}">
+                            <div class="@if($section == 'digital') bg-rqm-yellow text-rqm-dark @else text-rqm-yellow-dark  bg-rqm-light @endif p-3 rounded">Digital options</div>
+                        </a>
+                    @endif
+                    <a href="{{ route('admin.product.edit', [$basicProduct, 'images']) }}">
+                        <div class="@if($section == 'images') bg-rqm-yellow text-rqm-dark @else text-rqm-yellow-dark  bg-rqm-light @endif p-3 rounded">Images</div>
+                    </a>
+                </div>
+            </div>
+
             @if($section == 'basic')
                 @include('tailwind-ui.admin.product-basic-content', ['basicProduct' => $basicProduct])
             @elseif($section == 'offers')
