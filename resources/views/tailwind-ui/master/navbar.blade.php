3011ae068fd5754461812de7f15ef45f2fdf1a48
Pufferfish <RAm@some.com>
2021-05-01 03:45:30 +0800

    login draft

--- resources/views/tailwind-ui/master/navbar.blade.php
+++ resources/views/tailwind-ui/master/navbar.blade.php
@@ -53,11 +53,15 @@
                     <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
                 </svg>
                 <div class="absolute bg-rqm-dark group-hover:visible invisible p-1 right-32 rounded-b-lg w-1/12 z-30">
-                    <span class="block text-base px-4 py-2">{{auth() -> user()->username}}</span>
+                    @if(auth()->user())
+                        <span class="block text-base px-4 py-2">{{auth() -> user()->username}}</span>
+                    @endif
                     <hr class="border-t mx-2 border-rqm-yellow-darkest">
                     <a href="{{ route('profile.index') }}" class="block text-base hover:bg-pink-700 px-4 py-2 hover:grow">View Profile</a>
-                    @if(auth() -> user() -> isAdmin())
-                        <a href="{{ route('admin.index') }}" class="block text-base hover:bg-pink-700 px-4 py-2 hover:grow">Admin Panel</a>
+                    @if(auth()->user())
+                        @if(auth() -> user() -> isAdmin())
+                            <a href="{{ route('admin.index') }}" class="block text-base hover:bg-pink-700 px-4 py-2 hover:grow">Admin Panel</a>
+                        @endif
                     @endif
                     <hr class="border-t mx-2 border-rqm-yellow-darkest">
                     <form action="{{route('auth.signout.post')}}" method="post">
