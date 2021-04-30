d1f31868cd822d87210bc1d2f63d11ecddeee0ea
Pufferfish <RAm@some.com>
2021-04-30 15:53:44 +0800

    sidebr complete

--- resources/views/tailwind-ui/shops.blade.php
+++ resources/views/tailwind-ui/shops.blade.php
@@ -14,16 +14,50 @@
                     {{csrf_field()}}
                     <div>
                         <label for="search" class="text-rqm-yellow-darkest block">Search Terms</label>
-                        <input type="text" name="search" id="search" value="{{app('request')->input('query')}}" class="bg-rqm-lighter border rounded w-full">
+                        <input type="text" name="search" id="search" value="{{app('request')->input('query')}}" class="px-2 text-rqm-yellow bg-rqm-lighter border rounded w-full">
                     </div>
                     <div>
                         <label for="user" class="text-rqm-yellow-darkest block">User</label>
-                        <input type="text" name="user" id="user" value="{{app('request')->input('user')}}" class="bg-rqm-lighter border rounded w-full">
+                        <input type="text" name="user" id="user" value="{{app('request')->input('user')}}" class="px-2 text-rqm-yellow bg-rqm-lighter border rounded w-full">
                     </div>
                     <div>
                         <label for="category" class="text-rqm-yellow-darkest block">Category</label>
                         <select id="category" name="category" class="bg-rqm-light border py-0.5 rounded text-rqm-yellow-dark w-full">
                             <option selected value="any">Any</option>
+                            @foreach($categories as $category)
+                                <option value="{{$category->name}}" @if(app('request')->input('category') == $category->name) selected @endif>{{$category->name}}</option>
+                                @if($category -> children -> isNotEmpty())
+                                    @foreach($category->children as $child)
+                                        <option value="{{$child->name}}" @if(app('request')->input('category') == $child->name) selected @endif> > {{$child->name}}</option>
+                                        @if($child -> children -> isNotEmpty())
+                                            @foreach($child->children as $subChild)
+                                                <option value="{{$subChild->name}}" @if(app('request')->input('category') == $subChild->name) selected @endif> >> {{$subChild->name}}</option>
+                                            @endforeach
+                                        @endif
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+                    <div>
+                        <label for="product_type" class="text-rqm-yellow-darkest block">Type</label>
+                        <select id="product_type" name="product_type" class="bg-rqm-light border py-0.5 rounded text-rqm-yellow-dark w-full">
+                            <option selected value="all">All</option>
+                            <option value="digital" @if(app('request')->input('type') == 'digital') selected @endif>Digital</option>
+                            <option value="physical" @if(app('request')->input('type') == 'physical') selected @endif>Physical</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label class="text-rqm-yellow-darkest block">Price Range</label>
+                        <input type="number" name="minimum_price" id="" value="{{app('request')->input('price_min')}}" step="0.01" placeholder="Minimum price USD" class="px-2 text-rqm-yellow bg-rqm-lighter border rounded w-full">
+                        <input type="number" name="maximum_price" id="" value="{{app('request')->input('price_max')}}" step="0.01" placeholder="Maximum price USD" class="px-2 text-rqm-yellow mt-0.5 bg-rqm-lighter border rounded w-full">
+                    </div>
+                    <div>
+                        <label for="order_by" class="text-rqm-yellow-darkest block">Order By</label>
+                        <select id="order_by" name="order_by" class="bg-rqm-light border py-0.5 rounded text-rqm-yellow-dark w-full">
+                            <option @if(app('request')->input('order_by') == 'price_asc' ||app('request')->input('order_by') == null) selected @endif value="price_asc">Price: Low to High</option>
+                            <option @if(app('request')->input('order_by') == 'price_desc') selected @endif value="price_desc">Price: High to Low</option>
+                            <option @if(app('request')->input('order_by') == 'newest') selected @endif value="newest">Newest</option>
                         </select>
                     </div>
                 </form>
