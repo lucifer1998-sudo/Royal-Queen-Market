c155400c8e66651946bbf6c34ba37fd2cfcdcd3a
Pufferfish <nihondash1>
2021-05-07 01:14:55 +0500

    working on footer section

--- resources/views/tailwind-ui/master/footer.blade.php
+++ resources/views/tailwind-ui/master/footer.blade.php
@@ -1,32 +1,76 @@
-<footer class="flex justify-center text-center py-10">
-    <div>
-        <div class="flex justify-center">
-            <img src="{{URL::asset('/media/royal-queen-logo.png')}}" class="w-40" alt="">
-        </div>
-
-        <div class="pt-7 text-3xl text-rqm-yellow-darkest">
-            CATEGORIES
-        </div>
-
-        <div class="flex justify-center pt-2">
-            <a class="px-3 text-rqm-yellow hover:text-rqm-yellow-darkest" href="#">WEED</a>
-            <a class="px-3 text-rqm-yellow hover:text-rqm-yellow-darkest" href="#">HASH</a>
-            <a class="px-3 text-rqm-yellow hover:text-rqm-yellow-darkest" href="#">EDIBLES</a>
-            <a class="px-3 text-rqm-yellow hover:text-rqm-yellow-darkest" href="#">VAPES</a>
-            <a class="px-3 text-rqm-yellow hover:text-rqm-yellow-darkest" href="#">OTHERS</a>
-        </div>
-
-        <div class="flex justify-center pt-2">
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">WHO WE ARE</a>
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">STEPS</a>
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">BECOME A VENDOR</a>
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">SHIPPING</a>
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">PRIVACY POLICY</a>
-            <a class="px-3 text-gray-300 hover:text-gray-100" href="#">CONTACT US</a>
-        </div>
-
-        <div class="flex justify-center pt-6">
-            <div class="px-3 text-rqm-yellow-dark">© Royal Queen 2021</div>
-        </div>
-    </div>
-</footer>
+{{--<footer class="flex  py-10">--}}
+    <footer class="flex justify-center text-center py-10 px-10 ">
+            <div class="">
+                <img src="{{URL::asset('/media/royal-queen-logo.png')}}" class="w-40" alt="" style="height:auto;width:300px">
+            </div>
+            <div class="vl"></div>
+            <div class=" justify-center pt-2 flex-grow" >
+                    <ul>
+                        <h2 class="text-rqm-yellow-darkest">Categories</h2>
+                        <li class="text-white">WEED</li>
+                        <li class="text-white">HASH</li>
+                        <li class="text-white">Concentrate</li>
+                        <li class="text-white">Seeds</li>
+                        <li class="text-white">Others</li>
+                        <li class="text-white">Lorem</li>
+                    </ul>
+                </div>
+            <div class="justify-center pt-2 flex-grow">
+                    <ul>
+                        <h2 class="text-rqm-yellow-darkest">Help</h2>
+                        <li class="text-white">First Steps</li>
+                        <li class="text-white">Security</li>
+                        <li class="text-white">Orders</li>
+                        <li class="text-white">Payment</li>
+                        <li class="text-white">Shipping</li>
+                        <li class="text-white">Become a Vendor</li>
+                        <li class="text-white">Contact Us</li>
+                    </ul>
+                </div>
+            <div class="justify-center pt-2 flex-grow">
+                    <ul>
+                        <h2 class="text-rqm-yellow-darkest">Bitcoin Exchanges</h2>
+                        <li class="bg-gray text-white mb-2">USD
+                        <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">Euro
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">GRP
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">AUD
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">CMF
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">CAD
+                            <span>123</span>
+                        </li>
+                    </ul>
+                </div>
+            <div class="justify-center pt-2 mr-2 flex-grow" style="margin-left: 20px">
+                    <ul>
+                        <h2 class="text-rqm-yellow-darkest">Bitcoin Exchanges</h2>
+                        <li class="bg-gray text-white mb-2">USD
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">Euro
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">GRP
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">AUD
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">CMF
+                            <span>123</span>
+                        </li>
+                        <li class="bg-gray text-white mb-2">CAD
+                            <span>123</span>
+                        </li>
+                    </ul>
+                </div>
+    </footer>
