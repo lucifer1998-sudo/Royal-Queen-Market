de57be5bc2ead12cd778e723260658f550f420e8
Pufferfish <nihondash1>
2021-05-07 17:03:07 +0500

    fixed the reported issues

--- resources/views/auth/mnemonic.blade.php
+++ resources/views/auth/mnemonic.blade.php
@@ -6,20 +6,19 @@
 @section('content')
 
     <div class="flex ml-34rm py-7">
-        <div class="text-white text-center w-2/5">
+        <div class="text-white text-center w-3/5">
             <h4 class="text-rqm-yellow text-4xl">Mnemonic</h4>
             <div class="mt-3 text-center">
                 <div class="form-group">
                     <p>
-                        This is your mnemonik key. It consists out of {{config('marketplace.mnemonic_length')}} words.
-                        Please write
-                        them down. This is the only time they will be shown to you, and without them you cannot recover
-                        your account
-                        in case you lose password.
+                        We have created your mnemonic key for your added security.
+                        Please save and keep it in a safe place.
+                        You will need this to recover your account, in case you forget password.
+                        if you lose this, no one can help you recover you account and funds may be lost.
                     </p>
                 </div>
                 <div class="form-group py-4">
-                    <textarea name="" id="" cols="30" rows="10" readonly class="form-control">{{$mnemonic}}</textarea>
+                    <textarea name="" id="" cols="70" rows="10" readonly class="form-control">{{$mnemonic}}</textarea>
                 </div>
 {{--                <div class="form-group text-center">--}}
 {{--                    <a href="{{route('auth.signin')}}" class="btn btn-warning bg-rqm-yellow font-black py-2 text-rqm-dark w-full">Proceed to Sign In</a>--}}
