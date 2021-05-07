a83babb2a4b46a97dd38264e1b3ba57cdc2229bb
Pufferfish <nihondash1>
2021-05-08 02:12:47 +0500

    working on product page

--- resources/views/tailwind-ui/add-product.blade.php
+++ resources/views/tailwind-ui/add-product.blade.php
@@ -32,64 +32,20 @@
                             <form class="w-full max-w-6xl">
                                 <div class="flex flex-wrap -mx-3 mb-6">
                                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-first-name">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
                                             Products Name
                                         </label>
-                                        <input
-                                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                            id="grid-first-name" type="text" placeholder="Jane">
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Product Name">
                                     </div>
                                     <div class="w-full md:w-1/2 px-3">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-last-name">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
                                             Products Category
                                         </label>
-                                        <input
-                                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            id="grid-last-name" type="text" placeholder="Doe">
-                                    </div>
-                                </div>
-                                <div class="flex flex-wrap -mx-3 mb-6">
-                                    <div class="w-full px-3">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-password">
-                                            Password
-                                        </label>
-                                        <input
-                                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            id="grid-password" type="password" placeholder="******************">
-                                        <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd
-                                            like</p>
-                                    </div>
-                                </div>
-                                <div class="flex flex-wrap -mx-3 mb-2">
-                                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-city">
-                                            City
-                                        </label>
-                                        <input
-                                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            id="grid-city" type="text" placeholder="Albuquerque">
-                                    </div>
-                                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-state">
-                                            State
-                                        </label>
                                         <div class="relative">
-                                            <select
-                                                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                                id="grid-state">
-                                                <option>New Mexico</option>
-                                                <option>Missouri</option>
-                                                <option>Texas</option>
+                                            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                                <option>Option 1</option>
+                                                <option>option 2</option>
+                                                <option>Option 3</option>
                                             </select>
                                             <div
                                                 class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -101,15 +57,115 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                        <label
-                                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                            for="grid-zip">
-                                            Zip
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
+                                            Products Description
                                         </label>
-                                        <input
-                                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            id="grid-zip" type="text" placeholder="90210">
+                                        <textarea class="resize-x border rounded-md w-full h-full">Product Description ...</textarea>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3 py-5">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
+                                            Payment Rules
+                                        </label>
+                                        <textarea class="resize-x border rounded-md w-full h-full">Payment Rules ...</textarea>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
+                                            Supported types
+                                        </label>
+                                        <div class="relative">
+                                            <select multiple class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                                <option>Option 1</option>
+                                                <option>Option 2</option>
+                                                <option>Option 3</option>
+                                            </select>
+                                            <div
+                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
+                                            Supported coins
+                                        </label>
+                                        <div class="relative">
+                                            <select multiple class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                                <option>Option 1</option>
+                                                <option>Option 2</option>
+                                                <option>Option 3</option>
+                                            </select>
+                                            <div
+                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                        <label class="block uppercase tracking-wide text-white font-bold mb-2">
+                                            Quantity
+                                        </label>
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="number" placeholder="Quantity">
+                                    </div>
+                                    <div class="w-full md:w-1/2 px-3 ">
+                                        <label class="block uppercase tracking-wide text-white  font-bold mb-2">
+                                            Measure
+                                        </label>
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Measure">
+                                    </div>
+
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6 text-center">
+                                    <div class="w-full px-3">
+                                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                            Next
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <div class="bg-rqm-lighter p-5 rounded shadow w-full ">
+                                            <a href="#" class="text-2xl text-gray-400 block text-rqm-yellow">Price & Offers</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <div class="bg-rqm-lighter p-5 rounded shadow w-full ">
+                                            <a href="#" class="text-2xl text-gray-400 block text-rqm-yellow">Delivery Options</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6">
+                                    <div class="w-full px-3">
+                                        <div class="bg-rqm-lighter p-5 rounded shadow w-full ">
+                                            <a href="#" class="text-2xl text-gray-400 block text-rqm-yellow">Images</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-wrap -mx-3 mb-6 text-center">
+                                    <div class="w-full px-3">
+                                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded">
+                                            Add Product
+                                        </button>
                                     </div>
                                 </div>
                             </form>
