0d35d72fc84a004d519cba00298b5318a0748119
Pufferfish <nihondash1>
2021-05-07 14:36:51 +0500

    mnemonic page styling

--- resources/views/auth/mnemonic.blade.php
+++ resources/views/auth/mnemonic.blade.php
@@ -5,12 +5,10 @@
 
 @section('content')
 
-    <div class="row mt-5 text-white">
-        <div class="col-md-6 offset-md-3">
-            <h2>Mnemonic</h2>
-
-
-            <div class="mt-3">
+    <div class="flex ml-34rm py-7">
+        <div class="text-white text-center w-2/5">
+            <h4 class="text-rqm-yellow text-4xl">Mnemonic</h4>
+            <div class="mt-3 text-center">
                 <div class="form-group">
                     <p>
                         This is your mnemonik key. It consists out of {{config('marketplace.mnemonic_length')}} words.
@@ -20,16 +18,18 @@
                         in case you lose password.
                     </p>
                 </div>
-                <div class="form-group">
+                <div class="form-group py-4">
                     <textarea name="" id="" cols="30" rows="10" readonly class="form-control">{{$mnemonic}}</textarea>
                 </div>
-                <div class="form-group text-center">
-                    <a href="{{route('auth.signin')}}" class="btn btn-warning">Proceed to Sign In</a>
+{{--                <div class="form-group text-center">--}}
+{{--                    <a href="{{route('auth.signin')}}" class="btn btn-warning bg-rqm-yellow font-black py-2 text-rqm-dark w-full">Proceed to Sign In</a>--}}
+{{--                </div>--}}
+                <div class="flex justify-center text-lg text-rqm-yellow">
+                    <a href="{{route('auth.signin')}}" value="Log In" class="bg-rqm-yellow font-bold py-2 text-rqm-dark w-full">Proceed to Sign In</a>
                 </div>
             </div>
 
         </div>
     </div>
-
 
 @stop
