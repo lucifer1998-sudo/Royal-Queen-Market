b297e43dbba727c90929f6a0640322cd9a0af575
Pufferfish <nihondash1>
2021-05-08 16:25:38 +0500

    working on prod page

--- resources/views/includes/profile/basicform.blade.php
+++ resources/views/includes/profile/basicform.blade.php
@@ -67,11 +67,11 @@
         <div class="form-group">
             <label for="coins">Supported types:</label>
             <select name="types[]" id="types" multiple class="form-control">
-            
+
                 @foreach(\App\Purchase::$types as $type => $typeLongName)
-                    
+
                         <option value="{{ $type }}" {{ optional($basicProduct) -> supportsType($type) ? 'selected' : '' }}>{{ $typeLongName }}</option>
-                    
+
                 @endforeach
 
             </select>
@@ -127,15 +127,15 @@
         <div class="form-row justify-content-center">
             <div class="form-group col-md-3 text-center">
                 @if(request() -> is('profile/vendor/product/edit/*'))
-                    <button class="btn btn-outline-success" type="submit"><i class="far fa-save mr-2"></i> Save</button>
+                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded" type="submit"><i class="far fa-save mr-2"></i> Save</button>
                     <a href="{{ route('profile.vendor.product.edit', [$basicProduct, 'offers']) }}"
                        class="btn btn-outline-primary"><i class="fas fa-chevron-down mr-2"></i> Next</a>
                 @elseif(request() -> is('admin/product/*'))
-                    <button class="btn btn-outline-success" type="submit"><i class="far fa-save mr-2"></i> Save</button>
+                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded" type="submit"><i class="far fa-save mr-2"></i> Save</button>
                     <a href="{{ route('admin.product.edit', [$basicProduct, 'offers']) }}"
                        class="btn btn-outline-primary"><i class="fas fa-chevron-down mr-2"></i> Next</a>
                 @else
-                    <button class="btn btn-outline-primary" type="submit"><i class="fas fa-chevron-down mr-2"></i> Next
+                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded" type="submit"><i class="fas fa-chevron-down mr-2"></i> Next
                     </button>
                 @endif
             </div>
