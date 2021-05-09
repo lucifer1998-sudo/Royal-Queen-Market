9e049bd2cfc36eab4681af112da134be4b0fb1d7
Pufferfish <nihondash1>
2021-05-10 03:13:46 +0500

    currency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Marketplace\Utility\CoinConverter;
 use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Log;
@@ -31,5 +32,22 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+        view () -> composer ( '*', function ( $view ) {
+//            $EUR_price = 0.98;
+//            $req_url = 'https://api.exchangerate-api.com/v4/latest/USD';
+//            $response_json = file_get_contents($req_url);
+//            if(false !== $response_json) {
+//                try {
+//                    $response_object = json_decode($response_json);
+//                    $base_price = 1; // Your price in USD
+//                    $EUR_price = round(($base_price * $response_object->rates->EUR), 2);
+//                }
+//                catch(Exception $e) {
+//                    $EUR_price =  0.88;
+//                }
+//            }
+            $rate = CoinConverter::conversion('USD','EUR','1');
+            $view -> with(['usd_eur' => $rate]);
+        });
     }
 }
--- resources/views/includes/currency.blade.php
+++ resources/views/includes/currency.blade.php
@@ -1,2 +1,6 @@
-{{ \App\Marketplace\Utility\CurrencyConverter::convertToLocal($usdValue) }}
-{{ \App\Marketplace\Utility\CurrencyConverter::getSymbol(\App\Marketplace\Utility\CurrencyConverter::getLocalCurrency()) }}
+@if(isset ( auth() -> user() -> currency ) && auth() -> user() -> currency -> currency == 'eur')
+    {{ $usdValue * $usd_eur . ' £'  }}
+@else
+    {{ \App\Marketplace\Utility\CurrencyConverter::convertToLocal($usdValue) }}
+    {{ \App\Marketplace\Utility\CurrencyConverter::getSymbol(\App\Marketplace\Utility\CurrencyConverter::getLocalCurrency()) }}
+@endif
