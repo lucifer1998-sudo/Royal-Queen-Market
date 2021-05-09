f0c1cf6901479840e1c2d7c015c3e5b31fcfecef
Pufferfish <nihondash1>
2021-05-09 15:19:02 +0500

    fix sales page

--- resources/views/tailwind-ui/includes/purchases/components/feedback.blade.php
+++ resources/views/tailwind-ui/includes/purchases/components/feedback.blade.php
@@ -1,88 +1,200 @@
-<div class="col-md-12">
-    {{-- Feedback --}}
-    @if($purchase -> isDelivered() && $purchase -> isBuyer() && !$purchase -> hasFeedback())
-        <tr class="text-white">
-            <td colspan="2" class="">
-                <h4>Leave feedback</h4>
+<div class="w-full">
+    <div class="h-full">
+        <div class="bg-rqm-lighter p-5 rounded shadow w-full">
+            @if($purchase -> isDelivered() && $purchase -> isBuyer() && !$purchase -> hasFeedback())
+                <label class="text-2xl text-gray-400 block text-rqm-yellow">
+                    Leave Feedback
+                </label>
+                <hr>
                 <form action="{{ route('profile.purchases.feedback.new', $purchase) }}" method="POST">
                     {{ csrf_field() }}
-                    <div class="form-row">
-                        <div class="col-md-3 text-left">
-                            <label for="quality_rate">Quality:</label>
-                            <select name="quality_rate" id="quality_rate" class="form-control">
-                                @for($i=1; $i<=5; $i++)
-                                    <option value="{{ $i }}">
-                                        {{ $i }} {{ str_plural('star', $i) }}
-                                    </option>
-                                @endfor
-                            </select>
-
-                            <label for="communication_rate">Communication:</label>
-                            <select name="communication_rate" id="communication_rate" class="form-control">
-                                @for($i=1; $i<=5; $i++)
-                                    <option value="{{ $i }}">
-                                        {{ $i }} {{ str_plural('star', $i) }}
-                                    </option>
-                                @endfor
-                            </select>
-                            <label for="shipping_rate">Shipping:</label>
-                            <select name="shipping_rate" id="shipping_rate" class="form-control">
-                                @for($i=1; $i<=5; $i++)
-                                    <option value="{{ $i }}">
-                                        {{ $i }} {{ str_plural('star', $i) }}
-                                    </option>
-                                @endfor
-                            </select>
+                    <div class="flex">
+                        <div class="w-full  m-2">
+                            <div class="h-full">
+                                <label class="block uppercase tracking-wide text-white font-bold mb-2">Quality:</label>
+                                <select name="quality_rate" id="quality_rate"
+                                        class=" appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight bg-rqm-dark border-rqm-yellow-darkest p-3 text-rqm-white ">
+                                    @for($i=1; $i<=5; $i++)
+                                        <option value="{{ $i }}">
+                                            {{ $i }} {{ str_plural('star', $i) }}
+                                        </option>
+                                    @endfor
+                                </select>
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <label for="type">Type:</label>
-                            <select name="type" id="type" class="form-control">
-                                <option value="positive">Positive</option>
-                                <option value="neutral" selected>Neutral</option>
-                                <option value="negative">Negative</option>
-                            </select>
+                        <div class="w-full  m-2">
+                            <div class="h-full">
+                                <label
+                                    class="block uppercase tracking-wide text-white font-bold mb-2">Communication:</label>
+                                <select name="communication_rate" id="communication_rate"
+                                        class="form-control appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight bg-rqm-dark border-rqm-yellow-darkest p-3 text-rqm-white ">
+                                    @for($i=1; $i<=5; $i++)
+                                        <option value="{{ $i }}">
+                                            {{ $i }} {{ str_plural('star', $i) }}
+                                        </option>
+                                    @endfor
+                                </select>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <label for="comment">Comment:</label>
-                            <textarea name="comment" id="comment" rows="5" class="form-control"
-                                      placeholder="Place your comment here"></textarea>
-                            <button type="submit" class="btn btn-block btn-outline-primary mt-2">Leave
-                                feedback
-                            </button>
+                        <div class="w-full  m-2">
+                            <div class="h-full">
+                                <label class="block uppercase tracking-wide text-white font-bold mb-2">Shipping:</label>
+                                <select name="shipping_rate" id="shipping_rate"
+                                        class="form-control appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight bg-rqm-dark border-rqm-yellow-darkest p-3 text-rqm-white ">
+                                    @for($i=1; $i<=5; $i++)
+                                        <option value="{{ $i }}">
+                                            {{ $i }} {{ str_plural('star', $i) }}
+                                        </option>
+                                    @endfor
+                                </select>
+                            </div>
+                        </div>
+                        <div class="w-full  m-2">
+                            <div class="h-full">
+                                <label class="block uppercase tracking-wide text-white font-bold mb-2">Type:</label>
+                                <select name="type" id="type"
+                                        class="form-control appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight bg-rqm-dark border-rqm-yellow-darkest p-3 text-rqm-white ">
+                                    <option value="positive">Positive</option>
+                                    <option value="neutral" selected>Neutral</option>
+                                    <option value="negative">Negative</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-
-
+                    <div class="flex">
+                        <div class="w-full">
+                            <div class="h-full">
+                                <label class="block uppercase tracking-wide text-white font-bold mb-2">Comment:</label>
+                                <textarea name="comment" id="comment" rows="5"
+                                          class="appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight bg-rqm-dark border-rqm-yellow-darkest p-3 text-rqm-white "
+                                          placeholder="Place your comment here"></textarea>
+                                <button type="submit"
+                                        class="bg-rqm-yellow-dark font-extrabold px-5 py-1 rounded-3xl text-rqm-dark text-center mb-4">
+                                    Leave
+                                    feedback
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </form>
-            </td>
-        </tr>
 
-    @elseif($purchase -> isDelivered() && $purchase -> hasFeedback())
-        <tr class="text-white">
-            <td colspan="2">
-                <h4>Feedback by buyer</h4>
-                <ul class="list-group">
-                    <li class="list-group-item">
+            @elseif($purchase -> isDelivered() && $purchase -> hasFeedback())
+
+                <label class="text-2xl text-gray-400 block text-rqm-yellow">
+                    <h4>Feedback by buyer</h4>
+                </label>
+                <ul class="list-disc">
+                    <li class="text-white">
                         Qualtiy:
-                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> quality_rate])
+                        @include('tailwind-ui.includes.purchases.stars', ['stars' => $purchase -> feedback -> quality_rate])
                     </li>
-                    <li class="list-group-item">
+                    <li class="text-white">
                         Shipping:
-                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> shipping_rate])
+                        @include('tailwind-ui.includes.purchases.stars', ['stars' => $purchase -> feedback -> shipping_rate])
                     </li>
-                    <li class="list-group-item">
+                    <li class="text-white">
                         Communication:
-                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> communication_rate])
+                        @include('tailwind-ui.includes.purchases.stars', ['stars' => $purchase -> feedback -> communication_rate])
                     </li>
-                    <li class="list-group-item">
+                    <li class="text-white">
                         Type:
                         {{ $purchase->feedback->getType() }}
                     </li>
-                    <li class="list-group-item text-center">
+                    <li class="text-white text-center">
                         {{ $purchase -> feedback -> comment }}
                     </li>
                 </ul>
-            </td>
-        </tr>
-    @endif
+            @endif
+
+        </div>
+    </div>
 </div>
+
+{{--<div class="col-md-12">--}}
+{{--    --}}{{-- Feedback --}}
+{{--    @if($purchase -> isDelivered() && $purchase -> isBuyer() && !$purchase -> hasFeedback())--}}
+{{--        <tr class="text-white">--}}
+{{--            <td colspan="2" class="">--}}
+{{--                <h4>Leave feedback</h4>--}}
+{{--                <form action="{{ route('profile.purchases.feedback.new', $purchase) }}" method="POST">--}}
+{{--                    {{ csrf_field() }}--}}
+{{--                    <div class="form-row">--}}
+{{--                        <div class="col-md-3 text-left">--}}
+{{--                            <label for="quality_rate">Quality:</label>--}}
+{{--                            <select name="quality_rate" id="quality_rate" class="form-control">--}}
+{{--                                @for($i=1; $i<=5; $i++)--}}
+{{--                                    <option value="{{ $i }}">--}}
+{{--                                        {{ $i }} {{ str_plural('star', $i) }}--}}
+{{--                                    </option>--}}
+{{--                                @endfor--}}
+{{--                            </select>--}}
+
+{{--                            <label for="communication_rate">Communication:</label>--}}
+{{--                            <select name="communication_rate" id="communication_rate" class="form-control">--}}
+{{--                                @for($i=1; $i<=5; $i++)--}}
+{{--                                    <option value="{{ $i }}">--}}
+{{--                                        {{ $i }} {{ str_plural('star', $i) }}--}}
+{{--                                    </option>--}}
+{{--                                @endfor--}}
+{{--                            </select>--}}
+{{--                            <label for="shipping_rate">Shipping:</label>--}}
+{{--                            <select name="shipping_rate" id="shipping_rate" class="form-control">--}}
+{{--                                @for($i=1; $i<=5; $i++)--}}
+{{--                                    <option value="{{ $i }}">--}}
+{{--                                        {{ $i }} {{ str_plural('star', $i) }}--}}
+{{--                                    </option>--}}
+{{--                                @endfor--}}
+{{--                            </select>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-md-3">--}}
+{{--                            <label for="type">Type:</label>--}}
+{{--                            <select name="type" id="type" class="form-control">--}}
+{{--                                <option value="positive">Positive</option>--}}
+{{--                                <option value="neutral" selected>Neutral</option>--}}
+{{--                                <option value="negative">Negative</option>--}}
+{{--                            </select>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-md-6">--}}
+{{--                            <label for="comment">Comment:</label>--}}
+{{--                            <textarea name="comment" id="comment" rows="5" class="form-control"--}}
+{{--                                      placeholder="Place your comment here"></textarea>--}}
+{{--                            <button type="submit" class="btn btn-block btn-outline-primary mt-2">Leave--}}
+{{--                                feedback--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+
+
+{{--                </form>--}}
+{{--            </td>--}}
+{{--        </tr>--}}
+
+{{--    @elseif($purchase -> isDelivered() && $purchase -> hasFeedback())--}}
+{{--        <tr class="text-white">--}}
+{{--            <td colspan="2">--}}
+{{--                <h4>Feedback by buyer</h4>--}}
+{{--                <ul class="list-group">--}}
+{{--                    <li class="list-group-item">--}}
+{{--                        Qualtiy:--}}
+{{--                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> quality_rate])--}}
+{{--                    </li>--}}
+{{--                    <li class="list-group-item">--}}
+{{--                        Shipping:--}}
+{{--                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> shipping_rate])--}}
+{{--                    </li>--}}
+{{--                    <li class="list-group-item">--}}
+{{--                        Communication:--}}
+{{--                        @include('includes.purchases.stars', ['stars' => $purchase -> feedback -> communication_rate])--}}
+{{--                    </li>--}}
+{{--                    <li class="list-group-item">--}}
+{{--                        Type:--}}
+{{--                        {{ $purchase->feedback->getType() }}--}}
+{{--                    </li>--}}
+{{--                    <li class="list-group-item text-center">--}}
+{{--                        {{ $purchase -> feedback -> comment }}--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+{{--            </td>--}}
+{{--        </tr>--}}
+{{--    @endif--}}
+{{--</div>--}}
