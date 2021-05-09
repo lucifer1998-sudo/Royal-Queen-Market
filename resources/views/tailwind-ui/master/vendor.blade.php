c683351bcd0e3d41e66a6dc8c47cd19bfccbfe41
Pufferfish <nihondash1>
2021-05-09 17:43:56 +0500

    vendor page

--- resources/views/tailwind-ui/master/vendor.blade.php
+++ resources/views/tailwind-ui/master/vendor.blade.php
@@ -18,7 +18,7 @@
             <li class="px-2">{{ $vendor -> username }}</li>
         </ol>
     </nav>
-    <div class="flex {{$vendor->vendor->getProfileBg()}} text-center justify-content-center">
+    <div class="flex my-5 {{$vendor->vendor->getProfileBg()}} text-center justify-content-center">
         @include('tailwind-ui.includes.vendor.card')
     </div>
 
