87eeceb746f7921b99c250b399d6a765647c1120
Pufferfish <nihondash1>
2021-05-18 20:11:41 +0800

    navbar and footer on login

--- resources/views/tailwind-ui/master/main.blade.php
+++ resources/views/tailwind-ui/master/main.blade.php
@@ -22,7 +22,7 @@
 
 </head>
 <body class="bg-rqm-dark text-gray-600 work-sans leading-normal text-base tracking-normal">
-@if(\Route::currentRouteName() != 'auth.signin' &&  \Route::currentRouteName() != 'auth.signup' &&  \Route::currentRouteName() != 'a.forgot' &&  \Route::currentRouteName() != 'a.mnemonic')
+@if(\Route::currentRouteName() != 'auth.logins' &&  \Route::currentRouteName() != 'auth.signup' &&  \Route::currentRouteName() != 'a.forgot' &&  \Route::currentRouteName() != 'a.mnemonic')
     @include('tailwind-ui.master.navbar')
 
 @endif
@@ -33,7 +33,7 @@
         @yield('content')
     </div>
 </section>
-@if(\Route::currentRouteName() != 'auth.signin' &&  \Route::currentRouteName() != 'auth.signup'  &&  \Route::currentRouteName() != 'a.forgot' &&  \Route::currentRouteName() != 'a.mnemonic')
+@if(\Route::currentRouteName() != 'auth.logins' &&  \Route::currentRouteName() != 'auth.signup'  &&  \Route::currentRouteName() != 'a.forgot' &&  \Route::currentRouteName() != 'a.mnemonic')
 
 
     <div class="bg-rqm-lighter container inset-0.5 px-6 mx-auto mb-10 mt-10">
